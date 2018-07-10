6a84f0dc3a123ba0481ed574ea79e603b24248b7
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2018-07-10 13:20:01 -0300

    Encapsulate filters in a segment

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Segment } from 'semantic-ui-react'
 import PlayerFilters from '../containers/PlayerFilters'
 import PlayerResults from '../containers/PlayerResults'
 
 
 const Content = () => (
    <Container>
-      <PlayerFilters />
-      <PlayerResults />
+     <Segment>
+       <PlayerFilters />
+     </Segment>
+     <PlayerResults />
     </Container>
 );
 
